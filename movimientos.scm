6ba3866b6240b38ee7ccadeca72af4fafe613ed0
Heber <heberdanielorellana@gmail.com>
2019-05-28 00:43:09 -0600

    proyecto iniciado

---
+++ movimientos.scm
@@ -0,0 +1,11 @@
+(define matriz (vector (make-vector 4) (make-vector 4) (make-vector 4) (make-vector 4)))
+(define (movimientoArriba)
+
+    (define (ciclo1)
+
+    )
+)
+(define (opera-pares n n2)
+    (cond ((= n n2) consequent1)
+        (predicate2 consequent2))
+)
